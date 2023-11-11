4303eae2b3edafb0081cae53325c1b08ec304127
Lukaszwutkowski <64429535+Lukaszwutkowski@users.noreply.github.com>
2023-11-11 09:20:12 +0100

    started with controller class

--- src/main/java/com/homework2springboot/carrestapi/controller/CarController.java
+++ src/main/java/com/homework2springboot/carrestapi/controller/CarController.java
@@ -1,9 +1,16 @@
 package com.homework2springboot.carrestapi.controller;
 
+import com.homework2springboot.carrestapi.model.Car;
 import com.homework2springboot.carrestapi.service.CarServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "/cars")
@@ -14,6 +21,15 @@ public class CarController {
     public CarController(CarServiceImpl carService) {
         this.carService = carService;
     }
+
+    @GetMapping
+    public ResponseEntity<List<Car>> getCars(){
+        List<Car> allCars = carService.getAllCars();
+        return new ResponseEntity<>(allCars, HttpStatus.OK);
+    }
+
+
+
 
 
 }
